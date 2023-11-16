47abab7a13292134176031a06246e4b0d569e5cd
jal119 <105179174+jal119@users.noreply.github.com>
2023-11-16 11:43:40 -0600

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,5 +1,5 @@
 def call() {
     withSonarQubeEnv('sonar-server') {
-        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Youtube-video -Dsonar.projectKey=Youtube-video '''
+        sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
     }
 }
